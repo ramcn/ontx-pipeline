4d33ea30b08a20e7a886258adf1db993fa7dd975
ramcn <cn.ramachandra@gmail.com>
2019-12-03 14:10:48 +0530

    Delete hybrid-workflow.sh

--- hybrid-workflow.sh
+++
@@ -1,5 +0,0 @@
-cd dataset
-flappie  ../fast5_files/ > ont.fasta
-spades.py --careful --pe1-1 Kp2146_paired_1.fastq.gz --pe1-2 Kp2146_paired_2.fastq.gz -o spades -t 16
-darwin-xl ecoli_spades_50.fasta reads_50.fasta 0 0  > out.sam
-nanopolish variants --consensus -o polished.vcf -w "tig00000001:200000-202000" -r reads.fasta -b reads.sorted.bam -g draft.fa 
